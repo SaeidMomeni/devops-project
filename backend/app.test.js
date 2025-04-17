b3f62736cba8e976dffc24e5db7b8954b70817f4
saeid <ali@Alis-MacBook-Pro.local>
2025-04-17 12:56:05 +0330

    update test description and expected message

--- backend/app.test.js
+++ backend/app.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const app = require("./index");
 
 describe("GET /", () => {
-  it("باید متن 'Hello from backend with MongoDB!' برگردونه", async () => {
+  it("باید متن 'Hello World' برگردونه", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello from backend with MongoDB!");
+    expect(res.text).toBe("Hello World");
   });
 });
 
