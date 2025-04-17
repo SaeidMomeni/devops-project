7ddc8c0de0a999af06e3cefb4f844b2499aee899
saeid <ali@Alis-MacBook-Pro.local>
2025-04-17 12:28:43 +0330

    fix test message

--- backend/app.test.js
+++ backend/app.test.js
@@ -5,7 +5,7 @@ describe("GET /", () => {
   it("باید متن 'Hello World' برگردونه", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello World");
+    expect(res.text).toBe("Hello from backend with MongoDB!");
   });
 });
 
